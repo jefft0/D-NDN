e6d76e08cd1d963a4c71efe29e1f8f2850e34236
Jeff Thompson <jefft0@remap.ucla.edu>
2015-12-14 06:44:46 -0800

    encoding: Added EncodingException.

---
+++ src/net/named_data/dndn/encoding/encoding_exception.d
@@ -0,0 +1,35 @@
+﻿/*
+ * Copyright (C) 2015 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+module net.named_data.dndn.encoding.encoding_exception;
+
+/**
+ * EncodingException extends Exception to report errors in encoding.
+ */
+class EncodingException : Exception {
+  /**
+   * Create and EncodingException with the given message.
+   * Params:
+   * message = The exception message.
+   */
+  this(string message)
+  {
+    super(message);
+  }
+}
